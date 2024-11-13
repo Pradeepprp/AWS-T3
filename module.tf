63e4b01b2b3942c0fdb6cb43f1405c6daa8ebf06
Pradeepprp <105343547+Pradeepprp@users.noreply.github.com>
2024-11-13 11:58:50 +0000

    Add .gitignore to ignore unnecessary files

--- module.tf
+++
@@ -1,6 +0,0 @@
-module "parameters_inclusion" {
-  source             = "./code"                 
-  region             = "us-east-1"             
-  cidr_block         = "10.0.0.0/16"            
-  customer_gateway_ip = "203.0.113.12"
-}
